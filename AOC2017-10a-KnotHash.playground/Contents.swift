3efb7cff70522624d7cbed5ba01bb556c4c7ed86
Donna McCulloch <donnalea@mcchouse.com>
2017-12-10 00:44:56 -0800

    Updated Day 10 Part 2 description.

--- AOC2017-10a-KnotHash.playground/Contents.swift
+++ AOC2017-10a-KnotHash.playground/Contents.swift
@@ -1,5 +1,5 @@
-//: Advent of Code 2017 - Day 8 -
-// https://adventofcode.com/2017/day/8
+//: Advent of Code 2017 - Day 10 -
+// https://adventofcode.com/2017/day/10
 
 import Foundation
 import XCTest
