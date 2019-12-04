f1eeff6f3effd2724de4d48bf7a4601821e592cf
Donna McCulloch <donnalea@mcchouse.com>
2019-12-03 23:46:13 -0800

    Added String extension for reading characters and ranges from a string

--- 2019/AOC2019-XXa-Template.playground/Contents.swift
+++ 2019/AOC2019-XXa-Template.playground/Contents.swift
@@ -3,6 +3,36 @@
 
 import XCTest
 
+extension String {
+
+  var length: Int {
+    return count
+  }
+
+  subscript (i: Int) -> String {
+    return self[i ..< i + 1]
+  }
+
+  func substring(fromIndex: Int) -> String {
+    return self[min(fromIndex, length) ..< length]
+  }
+
+  func substring(toIndex: Int) -> String {
+    return self[0 ..< max(0, toIndex)]
+  }
+
+  subscript (r: Range<Int>) -> String {
+    let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                        upper: min(length, max(0, r.upperBound))))
+    let start = index(startIndex, offsetBy: range.lowerBound)
+    let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+    return String(self[start ..< end])
+  }
+
+}
+
+// ----------------------------------------------------------------------------------
+
 func calculate(input: String) -> Int {
   
   return 0
@@ -49,3 +79,5 @@ let input = """
 //let value = calculate(input: input)
 //print(value)
 // Correct Answer: ???
+
+
